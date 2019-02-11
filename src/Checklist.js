6b54062831b74c1c07f85c8f02b576c5149e1ad2
Oscar Dub <oscardub@gmail.com>
2019-02-11 17:19:31 +0000

    Create stub components

---
+++ src/Checklist.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Checklist.css";
+
+class Checklist extends Component {
+  render() {
+    return <div className="Checklist">Hello, world!</div>;
+  }
+}
+
+export default Checklist;
---
+++ src/ChecklistItem.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./ChecklistItem.css";
+
+class ChecklistItem extends Component {
+  render() {
+    return <div className="ChecklistItem">Hello, world!</div>;
+  }
+}
+
+export default ChecklistItem;
