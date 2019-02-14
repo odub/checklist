7efc909617bc88d52d8006da3158ea62665198dc
Oscar Dub <oscardub@gmail.com>
2019-02-14 12:01:02 +0000

    Fix CSS for small screens

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,10 @@
 export const colsToTemplate = cols =>
-  ["36px", ...cols.map(col => `${col.width || 1}fr`)].join(" ");
+  [
+    "36px",
+    ...cols.map(
+      col => `minMax(${(col.width || 1) * 120}px, ${col.width || 1}fr)`
+    )
+  ].join(" ");
 
 export const formatStatus = status => {
   switch (status) {
