c678d807a483dfdbc5092c6fb509b221f6891255
Oscar Dub <oscardub@gmail.com>
2019-02-16 18:13:48 +0000

    Fix docs build

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  webpackConfig: require("react-scripts/config/webpack.config"),
+  webpackConfig: require("react-scripts/config/webpack.config")("development"),
   components: "src/components/Checklist.js",
   usageMode: "expand",
   exampleMode: "hidden",
